c843151ff67a6b624c85fec45e032753e52b1c12
NikitaArtyushenko <112862626+NikitaArtyushenko@users.noreply.github.com>
2023-05-29 10:15:10 +0300

    Update change.php

--- change.php
+++ change.php
@@ -8,6 +8,19 @@ $db = new PDO('mysql:host=localhost;dbname=u52858', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // Пример HTTP-аутентификации.
+    // PHP хранит логин и пароль в суперглобальном массиве $_SERVER.
+    if (empty($_SERVER['PHP_AUTH_USER']) ||
+        empty($_SERVER['PHP_AUTH_PW']) ||
+        $_SERVER['PHP_AUTH_USER'] != 'admin' ||
+        md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
+      header('HTTP/1.1 401 Unanthorized');
+      header('WWW-Authenticate: Basic realm="mattakvshi"');
+      print('<h1>401 Требуется авторизация</h1>');
+      exit();
+    }
     $messages = array();
   if (!empty($_GET['save'])) {
     print('Спасибо, результаты сохранены.');
